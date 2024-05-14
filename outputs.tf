a937a583bdbd791f76c6a4a7e623d6a1dcb52f34
Olivier Hoareau <oha@greenberets.io>
2024-05-14 08:51:24 +0200

    fix: fix

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,12 @@
 output "endpoint" {
   value = aws_apigatewayv2_api.api.api_endpoint
 }
+output "execution_arn" {
+  value = aws_apigatewayv2_api.api.execution_arn
+}
+output "arn" {
+  value = aws_apigatewayv2_api.api.arn
+}
 output "dns" {
   value = replace(replace(aws_apigatewayv2_api.api.api_endpoint, "/^https?://([^/]*).*/", "$1"), "/^wss://([^/]*).*/", "$1")
 }
